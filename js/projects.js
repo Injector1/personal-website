328b696fb6996acf780ca410880efa9acb6c20ab
Injector <47394046+Injector1@users.noreply.github.com>
2020-08-16 22:56:46 +0500

    new features
    + fetching projects from github
    + new fonts
    + icons replaced by text

--- js/projects.js
+++ js/projects.js
@@ -2,53 +2,39 @@ new Vue({
     el: '#project-list',
     data() {
         return {
-            projects: [
-                {
-                    title: 'Project name',
-                    description: 'Project description',
-                    link: 'https://github.com/',
-                    tags: [
-                        'Tag 1',
-                        'Tag 2',
-                        'Tag 3'
-                    ]
-                },
-                {
-                    title: 'Project name',
-                    description: 'Project description',
-                    link: 'https://github.com/',
-                    tags: [
-                        'Tag 1',
-                        'Tag 2',
-                        'Tag 3'
-                    ]
-                },
-                {
-                    title: 'Project name',
-                    description: 'Project description',
-                    link: 'https://github.com/',
-                    tags: [
-                        'Tag 1',
-                        'Tag 2',
-                        'Tag 3'
-                    ]
-                },
-                {
-                    title: 'Project name',
-                    description: 'Project description',
-                    link: 'https://github.com/',
-                    tags: [
-                        'Tag 1',
-                        'Tag 2',
-                        'Tag 3'
-                    ]
-                },
-            ]
+            projects: []
         }
     },
     methods: {
         linkToGitHub(link) {
             window.location.href = link
+        },
+        filterProjects(projects) {
+            let result = []
+            projects.forEach((project) => {
+                result.push({
+                    "name": project.name,
+                    "description": project.description,
+                    "language": project.language,
+                    "link": `https://github.com/Injector1/${project.name}`
+                })
+            })
+            return result
         }
+    },
+    async mounted() {
+        await fetch("https://api.github.com/users/Injector1/repos")
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                this.projects = data
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+
+        this.projects = this.filterProjects(this.projects)
+        console.log(this.projects)
     }
 })
